1ccfd557f96643d6a037b801271d9472b66babf0
Odhisika <francisganyo64@gmail.com>
2023-10-16 07:46:06 +0000

    changes

--- client/src/components/Primary.jsx
+++ client/src/components/Primary.jsx
@@ -2,49 +2,62 @@ import React, { useEffect, useState } from 'react';
 import { Header, NavBar, SliderCad } from '../components';
 import { useSelector } from 'react-redux';
 
-const JHS = () => {
+const Primary = () => {
   const products = useSelector((state) => state.products);
-  const [JHS, setJHS] = useState(null);
+  const [displayedBooks, setDisplayedBooks] = useState(6); // Number of books to display initially
+  const [showAllBooks, setShowAllBooks] = useState(false);
+  const [allBooks, setAllBooks] = useState([]);
 
   useEffect(() => {
-    setJHS(
-      products?.filter(
-        (data) =>
-          data.product_category === 'JHS' &&
-          data.product_name &&
-          data.product_description &&
-          data.product_price &&
-          data.imageURL
-      )
+    const filteredBooks = products?.filter(
+      (data) =>
+        data.product_category === 'Primary' &&
+        data.product_name &&
+        data.product_description &&
+        data.product_price &&
+        data.imageURL
     );
+    setAllBooks(filteredBooks);
   }, [products]);
 
-  // Define the number of columns based on screen size
+  const handleExploreMore = () => {
+    setShowAllBooks(true);
+    setDisplayedBooks(allBooks.length);
+  };
+
+  const handleViewLess = () => {
+    setShowAllBooks(false);
+    setDisplayedBooks(6);
+  };
+
   const numColumns = {
-    sm: 1, // Small screens
-    md: 2, // Medium screens
-    lg: 3, // Large screens
-    xl: 4, // Extra-large screens
+    sm: 2, 
+    md: 2, 
+    lg: showAllBooks ? Math.min(4, allBooks.length) : 2, 
+    xl: showAllBooks ? Math.min(4, allBooks.length) : 2, 
   };
 
   return (
     <div>
-      <main className=" container w-screen min-h-screen flex items-center justify-start flex-col bg-primary">
+      <main className="container w-screen min-h-screen flex items-center justify-start flex-col bg-primary">
         <Header />
         <NavBar />
         <div className={`w-full items-start justify-start flex-wrap flex grid-cols-1 sm:grid-cols-${numColumns.sm} md:grid-cols-${numColumns.md} lg:grid-cols-${numColumns.lg} xl:grid-cols-${numColumns.xl} gap-6`}>
-          {JHS &&
-            JHS.map((data, i) => (
-              <div key={i} className="bg-white rounded-lg overflow-hidden shadow-md mt-1">
-                <SliderCad key={i} data={data} index={i} />
-              </div>
-            ))}
+          {allBooks.slice(0, displayedBooks).map((data, i) => (
+            <div key={i} className="bg-white rounded-lg overflow-hidden shadow-md mt-1">
+              <SliderCad key={i} data={data} index={i} />
+            </div>
+          ))}
         </div>
+        {!showAllBooks && allBooks.length > 6 && (
+          <button className='bg-red-500 text-white px-4 py-2 rounded-lg inline-block hover:bg-red-600 transition-colors' onClick={handleExploreMore}>Browse more books</button>
+        )}
+        {showAllBooks && (
+          <button className='bg-red-500 text-white px-4 py-2 rounded-lg inline-block hover:bg-red-600 transition-colors' onClick={handleViewLess}>View less</button>
+        )}
       </main>
     </div>
   );
 };
 
-export default JHS;
-
-
+export default Primary;
