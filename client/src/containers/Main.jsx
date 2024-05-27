44bb0da636e345d103f7ba93f2503371e2d6ccf6
FRANCIS GANYO <francisganyo64@gmail.com>
2024-05-27 09:51:12 +0000

    changes

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -19,17 +19,12 @@ const Main = () => {
   }, []);
   return <main className="w-screen min-h-screen flex items-center justify-start flex-col bg-primary">
     <Header />
-    <NavBar/>
     
     <div className=" w-full flex flex-col items-start justify-center mt-40 px-6 md:px-24 2xl:px-96 gap-12 pb-24">
       <Search/>
       <Home/>
       <HomeSlider/>
       <FilterSection/>
-      <div id="kindagerten"><KG/></div>
-      <div id="primary"><Primary/></div>
-      <div id="jhs"> <JHS/></div>
-      <div id="shs"><SHS/></div>
     </div>
     <Footer/>
     {isCart && <Cart/>}
