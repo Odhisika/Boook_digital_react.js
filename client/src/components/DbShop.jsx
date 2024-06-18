98c17338cd191a3330e3721a4168ddeaa79f3393
Francis Ganyo <francisganyo64@gmail.com>
2024-06-18 16:24:50 +0000

    changes

--- client/src/components/DbShop.jsx
+++ client/src/components/DbShop.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react'
-import { statuses,Primary,JHS,SHS, statush } from '../utils/styles'
-import {Spinner} from '../components'
-import { FaCloudUploadAlt, MdDelete } from '../asset/icons'
-import { deleteObject, getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
-import { storage } from '../config/firebase.config'
-import { useDispatch, useSelector } from 'react-redux'
-import {alertDanger, alertNull, alertSuccess} from "../context/actions/alertActions";
-import { buttonClick } from '../animations'
-import {motion, useInstantTransition} from "framer-motion"
-import { addNewProduct, getAllProducts } from '../api'
-import {setAllProducts} from "../context/actions/productActions"
-import { useSubmit } from 'react-router-dom'
+import React, { useState } from 'react';
+import { statuses, Primary, JHS, SHS, statush } from '../utils/styles';
+import { Spinner } from '../components';
+import { FaCloudUploadAlt, MdDelete } from '../asset/icons';
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../config/firebase.config';
+import { useDispatch, useSelector } from 'react-redux';
+import { alertDanger, alertNull, alertSuccess } from "../context/actions/alertActions";
+import { buttonClick } from '../animations';
+import { motion } from "framer-motion";
+import { addNewProduct, getAllProducts } from '../api';
+import { setAllProducts } from "../context/actions/productActions";
 
 const DbShop = () => {
   const dispatch = useDispatch();
   
-  const [itemName, setItemName] = useState("")
-  const [description, setDescription] = useState("")
-  const [category, setcategory] = useState(null)
-  const [publishers, setPublishers] = useState(''); // Corrected state variable name
-  const [authors, setAuthors] = useState('');
-  const [price, setprice] = useState("")
-  const [quantity_in_stock, setquantity_in_stock] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [progress, setprogress] = useState(null);
-  const [imageDownloadURL, setImageDownloadURL] = useState(null)
-  const alert =useSelector((state)=>state.alert);
+  const [itemName, setItemName] = useState("");
+  const [description, setDescription] = useState("");
+  const [color, setColor] = useState("");
+  const [suppliers, setSuppliers] = useState(""); 
+  const [category, setCategory] = useState("");
+  const [sizes, setSizes] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantityInStock, setQuantityInStock] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(null);
+  const [imageDownloadURL, setImageDownloadURL] = useState(null);
+  const alert = useSelector((state) => state.alert);
 
-  
-  
-  
   const uploadImage = (e) => {
     setIsLoading(true);
     const imageFile = e.target.files[0];
     const storageRef = ref(storage, `images/${Date.now()}_${imageFile.name}`);
-  
+
     const uploadTask = uploadBytesResumable(storageRef, imageFile);
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        setprogress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
       },
       (error) => {
         dispatch(alertDanger(`Error: ${error}`));
@@ -51,9 +48,9 @@ const DbShop = () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setImageDownloadURL(downloadURL);
           setIsLoading(false);
-          setprogress(null);
-          dispatch(alertSuccess("Image Uploaded to cloud "))
-            setTimeout(() => {
+          setProgress(null);
+          dispatch(alertSuccess("Image Uploaded to cloud"));
+          setTimeout(() => {
             dispatch(alertNull());
           }, 3000);
         });
@@ -61,180 +58,193 @@ const DbShop = () => {
     );
   };
 
-  const deleteImageFromFirebase =()=>{
-    setIsLoading(true)
-    const deleteRef =ref(storage,imageDownloadURL)
+  const deleteImageFromFirebase = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageDownloadURL);
     
-    deleteObject(deleteRef).then(()=>{
-      setImageDownloadURL(null)
-      setIsLoading(false)
-      dispatch(alertSuccess("Image deleted successfully"))
-          setTimeout(() => {
-            dispatch(alertNull());
-          }, 3000);
-    })
-  }
+    deleteObject(deleteRef).then(() => {
+      setImageDownloadURL(null);
+      setIsLoading(false);
+      dispatch(alertSuccess("Image deleted successfully"));
+      setTimeout(() => {
+        dispatch(alertNull());
+      }, 3000);
+    });
+  };
 
   const saveNewData = () => {
     const data = {
       product_name: itemName,
       product_description: description,
+      product_color: color,
+      product_sizes: sizes,
       product_category: category,
-      product_publishers: publishers, 
-      product_authors: authors, 
+      product_suppliers: suppliers,
       product_price: price,
-      quantity_in_stock: quantity_in_stock,
+      quantity_in_stock: quantityInStock,
       imageURL: imageDownloadURL,
-
     };
 
     addNewProduct(data)
       .then((res) => {
-        console.log(res)
+        console.log(res);
         dispatch(alertSuccess("New item added successfully"));
         setTimeout(() => {
           dispatch(alertNull());
         }, 3000);
         setImageDownloadURL(null);
         setItemName("");
-        setDescription("")
-        setPublishers(''); // Reset the state variable
-        setAuthors('');
-        setprice("");
-        setquantity_in_stock("");
-        setcategory(null);
-        dispatch(alertNull())
-      })
-    getAllProducts().then((data)=>{
+        setDescription("");
+        setSuppliers('');
+        setSizes('');
+        setPrice("");
+        setQuantityInStock("");
+        setCategory("");
+        dispatch(alertNull());
+      });
+    getAllProducts().then((data) => {
       dispatch(setAllProducts(data));
-    }) 
+    });
   };
-  
-     
-  
-
 
   return (
-    <div className="flex items-center justify-center flex-col pt-6 px-24 w-full ">
-      <div className=" border border-gray-200 rounded-md p-4 w-full flex flex-col items-center justify-center gap-4">
-        <InputValueField type="text"
-         placeholder={"Book name here"}
-         stateFunc={setItemName} 
-         stateValue={itemName}/>
+    <div className="flex items-center justify-center flex-col pt-6 px-24 w-full">
+      <div className="border border-gray-200 rounded-md p-4 w-full flex flex-col items-center justify-center gap-4">
+        <InputValueField
+          type="text"
+          placeholder={"Product name here"}
+          stateFunc={setItemName}
+          stateValue={itemName}
+        />
 
         <InputValue
           type="text"
-          placeholder="Book description (max 100 words)"
+          placeholder="Product description (max 100 words)"
           stateFunc={setDescription}
           stateValue={description}
           maxLength={100}
         />
-        
+
         <InputValueField
-        type="text"
-        placeholder="Publishers"
-        stateValue={publishers}
-        stateFunc={setPublishers}
-      />
+          type="text"
+          placeholder="Suppliers"
+          stateValue={suppliers}
+          stateFunc={setSuppliers}
+        />
 
-      <InputValueField
-        type="text"
-        placeholder="Authors"
-        stateValue={authors}
-        stateFunc={setAuthors}
-      />
+        <InputValueField
+          type="text"
+          placeholder="Color"
+          stateValue={color}
+          stateFunc={setColor}
+        />
 
+        <div className="w-full flex items-center justify-around gap-3 flex-wrap">
+          {statush && statush.map(data => (
+            <p
+              key={data.id}
+              onClick={() => setCategory(data.category)}
+              className={`px-4 py-3 rounded-md text-xl text-textColor font-semibold cursor-pointer hover:shadow-md border border-gray-300 backdrop:blur-md ${data.category === category ? "bg-red-400 text-primary" : "bg-transparent"}`}
+            >
+              {data.title}
+            </p>
+          ))}
+        </div>
 
+        <InputValueField
+          type="text"
+          placeholder={"Book price here"}
+          stateFunc={setPrice}
+          stateValue={price}
+        />
 
-      <div className=" w-full flex items-center justify-around gap-3 flex-wrap">
-        {statush && statush?.map(data =>(
-          <p key={data.id} onClick={() =>setcategory(data.category)}
-           className={` px-4 py-3 rounded-md text-xl text-textColor font-semibold cursor-pointer hover:shadow-md border border-gray-300 backdrop:blur-md ${data.category === category ? " bg-red-400 text-primary": " bg-transparent"}`}>
-            {data.title}</p>
-        ))}
-
-      </div>
-      <InputValueField type="int"
-         placeholder={"Book price here"}
-         stateFunc={setprice} 
-         stateValue={price}/>
-
-       <InputValueField type="number"
-        placeholder={"Book Quantity "}
-         stateFunc={setquantity_in_stock} 
-         stateValue={quantity_in_stock}/>
-          
-          <div className=" w-full bg-card backdrop-blur-md h-370 rounded-md border-2 border-dotted border-gray-300 cursor-pointer">
-            {isLoading ? <div className=" w-full h-full flex flex-col items-center justify-evenly px-24"><Spinner/>
-             { Math.round(progress>0)&&(
-              <div className=" w-full flex flex-col items-center justify-center gap-2">
-                <div className=" justify-between w-full flex">
-                  <span className=" text-base font-medium text-textColor">progress</span>
-                  <span className=" text-sm font-medium text-textColor"> {Math.round(progress)>0 &&(<>{`${Math.round(progress)}%`}</>)}</span>
-                 
-                </div>
-                  <div className=' w-full bg-gray-200 rounded-full h-2.5'>
-                    <div className=" bg-red-600 h-2.5 rounded-full transition-all duration-300 ease-in-out" style={{
-                      width:`${Math.round(progress)}%` 
-                    }}></div>
+        <InputValueField
+          type="number"
+          placeholder={"Book Quantity"}
+          stateFunc={setQuantityInStock}
+          stateValue={quantityInStock}
+        />
+        
+        <div className="w-full bg-card backdrop-blur-md h-370 rounded-md border-2 border-dotted border-gray-300 cursor-pointer">
+          {isLoading ? (
+            <div className="w-full h-full flex flex-col items-center justify-evenly px-24">
+              <Spinner />
+              {Math.round(progress) > 0 && (
+                <div className="w-full flex flex-col items-center justify-center gap-2">
+                  <div className="justify-between w-full flex">
+                    <span className="text-base font-medium text-textColor">Progress</span>
+                    <span className="text-sm font-medium text-textColor">{Math.round(progress)}%</span>
                   </div>
-              
-              </div>
-             )}
-            </div>:<>{!imageDownloadURL ? <>
-            <label>
-              <div className=" w-full h-full flex flex-col items-center justify-center cursor-pointer">
-                <div className=" items-center justify-center flex flex-col cursor-pointer">
-                  <p className=" font-bold text-4xl">
-                    <FaCloudUploadAlt  className=" rotate-0"/>
-                  </p>
-                  <p className=" text-lg text-textColor"> Click here to upload image </p>
+                  <div className="w-full bg-gray-200 rounded-full h-2.5">
+                    <div
+                      className="bg-red-600 h-2.5 rounded-full transition-all duration-300 ease-in-out"
+                      style={{ width: `${Math.round(progress)}%` }}
+                    ></div>
+                  </div>
                 </div>
-              </div>
-              <input
-                type='file'
-                name='upload-image'
-                accept='image/*'
-                onChange={uploadImage}
-                className=' w-0 h-0'
-              />
-            </label>
-            </>:<>
-            <div className=" relative w-full h-full overflow-hidden rounded-md">
-              <motion.img
-              whileHover={{scale: 1.15}}
-              src={imageDownloadURL}
-              className=" w-full h-full object-cover"/>
-              <motion.button
-               {...buttonClick}
-               type ="button" className=" absolute top-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out "
-               onClick={()=> deleteImageFromFirebase (imageDownloadURL)}
-               >
-               <MdDelete className=' -rotate-0' />
-              </motion.button>
-             
-
-
+              )}
             </div>
-            
-          </>}</>}
-          </div>
-          <motion.button onClick={saveNewData} {...buttonClick} className=' w-9/12 py-2 rounded-md bg-red-400 text-primary hover:bg-red-500 cursor-pointer'>
-            Submit
-          </motion.button>
+          ) : (
+            <>
+              {!imageDownloadURL ? (
+                <label>
+                  <div className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="items-center justify-center flex flex-col cursor-pointer">
+                      <p className="font-bold text-4xl">
+                        <FaCloudUploadAlt className="rotate-0" />
+                      </p>
+                      <p className="text-lg text-textColor">Click here to upload image</p>
+                    </div>
+                  </div>
+                  <input
+                    type='file'
+                    name='upload-image'
+                    accept='image/*'
+                    onChange={uploadImage}
+                    className='w-0 h-0'
+                  />
+                </label>
+              ) : (
+                <div className="relative w-full h-full overflow-hidden rounded-md">
+                  <motion.img
+                    whileHover={{ scale: 1.15 }}
+                    src={imageDownloadURL}
+                    className="w-full h-full object-cover"
+                  />
+                  <motion.button
+                    {...buttonClick}
+                    type="button"
+                    className="absolute top-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+                    onClick={() => deleteImageFromFirebase(imageDownloadURL)}
+                  >
+                    <MdDelete className='rotate-0' />
+                  </motion.button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+        
+        <motion.button onClick={saveNewData} {...buttonClick} className='w-9/12 py-2 rounded-md bg-red-400 text-primary hover:bg-red-500 cursor-pointer'>
+          Submit
+        </motion.button>
       </div>
-     </div>
-  )
-}
+    </div>
+  );
+};
 
-export const InputValueField =({type,placeholder, stateValue,stateFunc}) =>{
-  return <>
-      <input
-       type={type} placeholder={placeholder}
-       className=" w-full px-4 py-3 bg-slate-100 shadow-md outline-none rounded-md border border-gray-300 focus:border-red-500" value={stateValue}
-      onChange={(e)=>stateFunc(e.target.value)}/>
-  </>;
-}
+export const InputValueField = ({ type, placeholder, stateValue, stateFunc }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className="w-full px-4 py-3 bg-slate-100 shadow-md outline-none rounded-md border border-gray-300 focus:border-red-500"
+      value={stateValue}
+      onChange={(e) => stateFunc(e.target.value)}
+    />
+  );
+};
+
 export const InputValue = ({ type, placeholder, stateValue, stateFunc, maxLength }) => {
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
@@ -258,6 +268,5 @@ export const InputValue = ({ type, placeholder, stateValue, stateFunc, maxLength
     </>
   );
 };
-
 
 export default DbShop;
